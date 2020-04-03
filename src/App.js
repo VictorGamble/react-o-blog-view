03db19792d312160d8ac72627dca7a0519465382
Victor <Victor.Gamble@outlook.com>
2020-04-03 13:25:57 -0400

    imported EntriesList component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-
+import EntriesList from './components/EntryLists'
 
 function App() {
   return (
     <div className="App">
-
+      <EntriesList/>
     </div>
   );
 }
